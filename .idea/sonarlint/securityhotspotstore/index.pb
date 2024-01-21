99b3ab2dd8d5d2220a0b339a6b73eda314a6461a
Cesar <cesar.a.cortes.94@gmail.com>
2024-01-21 14:43:48 -0600

    Primer commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+`
+0src/main/java/org/pildorasInformaticas/Main.java,1\d\1db4c076b282b0bd220e86a541d890fba4717fd1
---
+++ src/main/java/org/pildorasInformaticas/Main.java
@@ -0,0 +1,17 @@
+package org.pildorasInformaticas;
+
+public class Main {
+    /*
+
+    1.- JRE (Java Runtime Enviroment), antes la JVM
+    2.- Java compila el codigo y lo interpreta con la ayuda de la JVM
+    3.- Compilar: pasar de codigo fuente a codigo maquina
+    4.- Solucion para que java sea multiplataforma: codigo fuente, despues
+    se hace un archivo intermedio de bytecodes
+
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
